b1a6f527172c2192c1366c4213f05dab62222517
Richard.Harper <Richard.Harper@working-age.digital.dwp.gov.uk>
2020-12-01 15:19:42 +0000

    Adding default 5 retry attempts with -m flag to override

--- main.go
+++ main.go
@@ -27,13 +27,15 @@ var opts struct {
 	TerrafilePath string `short:"f" long:"terrafile_file" default:"./Terrafile" description:"File path to the Terrafile file"`
 
 	WaitTime string `short:"w" long:"wait_time" default:"2" description:"Number of seconds to wait between each git clone"`
+
+	MaxAttempts string `short:"m" long:"max_attempts" default:"5" description:"Maximum number of attempts to git clone"`
 }
 
 // To be set by goreleaser on build
 var (
-	version = "InfinityWorksConsulting-v1.0.0"
-	commit  = "Adding wait time as an optional flag"
-	date    = "Friday 6th November 2020"
+	version = "InfinityWorksConsulting-v1.0.1"
+	commit  = "Adding default 5 retry attempts with -m flag to override"
+	date    = "Tuesday 1st December 2020"
 )
 
 func init() {
@@ -41,13 +43,20 @@ func init() {
 	log.AddHook(stdemuxerhook.New(log.StandardLogger()))
 }
 
-func gitClone(repository string, version string, moduleName string) {
+func gitClone(repository string, version string, moduleName string, maxAttempts int) {
 	log.Printf("[*] Checking out %s of %s \n", version, repository)
-	cmd := exec.Command("git", "clone", "--single-branch", "--depth=1", "-b", version, repository, moduleName)
-	cmd.Dir = opts.ModulePath
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalln(err)
+	for i := 1; i <= maxAttempts; i++ {
+		cmd := exec.Command("git", "clone", "--single-branch", "--depth=1", "-b", version, repository, moduleName)
+		cmd.Dir = opts.ModulePath
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("WARN: attempt %d to fetch %s at version %s \n", i, repository, version)
+			if i == maxAttempts {
+				log.Fatalln(err)
+			}
+		} else {
+			break;
+		}
 	}
 }
 
@@ -78,6 +87,12 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	// Parse Max Attempts
+	maxAttempts, err := strconv.Atoi(opts.MaxAttempts)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	// Clone modules
 	var wg sync.WaitGroup
 	_ = os.RemoveAll(opts.ModulePath)
@@ -87,7 +102,7 @@ func main() {
 		time.Sleep(time.Duration(waitTime) * time.Second)
 		go func(m module, key string) {
 			defer wg.Done()
-			gitClone(m.Source, m.Version, key)
+			gitClone(m.Source, m.Version, key, maxAttempts)
 			_ = os.RemoveAll(filepath.Join(opts.ModulePath, key, ".git"))
 		}(mod, key)
 	}
